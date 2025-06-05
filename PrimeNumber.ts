2c14dd0f2f8e786c59a594e0811796ed7e513cad
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2025-06-06 00:03:25 +0530

    added user input

--- PrimeNumber.ts
+++ PrimeNumber.ts
@@ -1,16 +1,31 @@
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 function isPrime(num: number): boolean {
-    if (num < 2 ) return false;
-    for (let i=2;i <= Math.sqrt(num); i++){
-        if(num%i === 0 ) return false;
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
     }
     return true;
 }
 
-let prime = isPrime(5)
+rl.question("Enter number to check prime: ", (input: string) => {
+    const num = parseInt(input);
 
-if(prime) {
-    console.log("Number is Prime Number");
-} else {
-    console.log("Not a Prime Number.");
-}
+    if (isNaN(num)) {
+        console.log("Please enter a valid number.");
+    } else {
+        let prime = isPrime(num);
+        if (prime) {
+            console.log("Number is Prime Number");
+        } else {
+            console.log("Not a Prime Number.");
+        }
+    }
+
+    rl.close();
+});
